684af989815ce2a2769f22815ec8896a4830af1e
Bray Stiens <bray.stiens@werner.com>
2025-04-04 10:18:42 -0400

    Add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RF-V1/', 
+  base: '/RestaurantFinder/', // Updated to match repository name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
